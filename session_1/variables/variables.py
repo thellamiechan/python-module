6babd358dd8c2aa53ad62037d69c918d65e82498
Julie E Powell <JEPowell@proton.me>
2023-05-30 20:26:28 +1000

    first night lesson

---
+++ session_1/variables/variables.py
@@ -0,0 +1,33 @@
+#create a variable named first_name 
+first_name = "Julie"
+last_name = "Powell"
+programming_language = "Python"
+
+#Data Types - text data, numerical data
+#text data - string, use quotes
+today = "tuesday"
+# print(today)
+# print("today is {today}")
+# print(type(today))
+
+
+#numerical data types
+#integer - whole number
+parkers_age = 4
+# print(type(parkers_age))
+# print("Parker is turning {parkers_age+1}")
+
+# height = 155
+# print(f"my height is {height/100}")
+
+distance = "5000"
+# print(int(distance) + 8)
+# print(distance + str(8))
+
+# input("hello")
+# dog_name = input("What's the dog's name?")
+# print(f"Nice to meet you {dog_name}")
+
+#Input data stores the data as a string
+# birth_year =input("What year were you born?")
+# print(f"you are {2023 - int(birth_year)}")
